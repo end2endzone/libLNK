26aa9a47f4b9afe874cf6d527805ec6618057edf
Antoine <end2endzone@gmail.com>
2018-02-09 18:06:52 -0500

    Fixed test_script.bat file for uploading test results to appveyor.

--- ci/appveyor/test_script.bat
+++ ci/appveyor/test_script.bat
@@ -24,7 +24,7 @@ echo =======================================================================
 echo Uploading test results to AppVeyor
 echo =======================================================================
 set TEST_RESULT_URL=https://ci.appveyor.com/api/testresults/junit/%APPVEYOR_JOB_ID%
-set TEST_RESULT_FILE=%CD%\msbuildreorderTest.x86.release.xml
+set TEST_RESULT_FILE=%CD%\libLNK_unittest.x86.release.xml
 echo TEST_RESULT_URL=%TEST_RESULT_URL%
 echo TEST_RESULT_FILE=%TEST_RESULT_FILE%
 powershell "(New-Object 'System.Net.WebClient').UploadFile($($env:TEST_RESULT_URL), $($env:TEST_RESULT_FILE))"
