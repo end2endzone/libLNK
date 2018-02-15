1a89c7d292e65a7da98f895a3d8c5ec1485d1864
Antoine <end2endzone@gmail.com>
2018-02-14 21:10:21 -0500

    Implemented issue #4: Rename test files to match test names.

--- src/libLNK_unittest/TestLNK.cpp
+++ src/libLNK_unittest/TestLNK.cpp
@@ -11,6 +11,16 @@
 
 gTestHelper & hlp = gTestHelper::getInstance();
 
+
+std::string getTestLink()
+{
+  std::string file;
+  file.append("./tests/");
+  file.append(hlp.getTestCaseName());
+  file.append(".lnk");  
+  return file;
+}
+
 std::string getExpectedFilePath()
 {
   std::string file;
@@ -116,6 +126,9 @@ void TestLNK::TearDown()
 
 TEST_F(TestLNK, testCreateSimpleLink)
 {
+  //Build test case link file
+  std::string lnkFilePath = getTestLink();
+
   //test creation identical
   lnk::LinkInfo info;
   info.target = "C:\\Program Files\\7-Zip\\History.txt";
@@ -126,18 +139,17 @@ TEST_F(TestLNK, testCreateSimpleLink)
   info.customIcon.index = 5;
   info.hotKey = lnk::LNK_NO_HOTKEY;
 
-  const char * linkFilename = ".\\tests\\Simple.txt.copy.lnk";
-  bool success = createLink(linkFilename, info);
+  bool success = createLink(lnkFilePath.c_str(), info);
   ASSERT_TRUE( success == true );
 
   //run the link
   std::string openCommandLine = "start \"\" ";
-  openCommandLine += linkFilename;
+  openCommandLine += lnkFilePath;
   system(openCommandLine.c_str());
   ASSERT_TRUE( findAndCloseWindow("History.txt - Notepad") );
 
   //test command
-  std::string command = lnk::getLinkCommand(linkFilename);
+  std::string command = lnk::getLinkCommand(lnkFilePath.c_str());
   std::string expectedCommand = info.target;
   expectedCommand += " ";
   expectedCommand += info.arguments;
@@ -147,16 +159,8 @@ TEST_F(TestLNK, testCreateSimpleLink)
   {
     lnk::LinkInfo info;
     bool success = false;
-    success = lnk::getLinkInfo(linkFilename, info);
+    success = lnk::getLinkInfo(lnkFilePath.c_str(), info);
     ASSERT_TRUE( success == true );
-    LinkInfoDebug d = {
-      info.target.c_str(),
-      info.networkPath.c_str(),
-      info.arguments.c_str(),
-      info.description.c_str(),
-      info.workingDirectory.c_str(),
-      info.customIcon.filename.c_str(),
-    };
     ASSERT_TRUE( info.target == "C:\\Program Files\\7-Zip\\History.txt" );
     ASSERT_TRUE( info.networkPath == "" );
     ASSERT_TRUE( info.arguments == "\"this is the arguments\"" );
@@ -169,6 +173,9 @@ TEST_F(TestLNK, testCreateSimpleLink)
 
 TEST_F(TestLNK, testCreateCustomLink)
 {
+  //Build test case link file
+  std::string lnkFilePath = getTestLink();
+
   //test creation
   lnk::LinkInfo info;
   info.target = "C:\\WINDOWS\\system32\\cmd.exe";
@@ -180,12 +187,11 @@ TEST_F(TestLNK, testCreateCustomLink)
   info.customIcon.index = 0;
   info.hotKey = lnk::LNK_NO_HOTKEY;
 
-  const char * linkFilename = "./tests/testcreate.copy.lnk";
-  bool success = lnk::createLink(linkFilename, info);
+  bool success = lnk::createLink(lnkFilePath.c_str(), info);
   ASSERT_TRUE( success == true );
 
   //test command
-  std::string command = lnk::getLinkCommand(linkFilename);
+  std::string command = lnk::getLinkCommand(lnkFilePath.c_str());
   std::string expectedCommand = info.target;
   expectedCommand += " ";
   expectedCommand += info.arguments;
@@ -195,16 +201,8 @@ TEST_F(TestLNK, testCreateCustomLink)
   {
     lnk::LinkInfo info;
     bool success = false;
-    success = lnk::getLinkInfo(linkFilename, info);
+    success = lnk::getLinkInfo(lnkFilePath.c_str(), info);
     ASSERT_TRUE( success == true );
-    LinkInfoDebug d = {
-      info.target.c_str(),
-      info.networkPath.c_str(),
-      info.arguments.c_str(),
-      info.description.c_str(),
-      info.workingDirectory.c_str(),
-      info.customIcon.filename.c_str(),
-    };
     ASSERT_TRUE( info.target == "C:\\WINDOWS\\system32\\cmd.exe" );
     ASSERT_TRUE( info.networkPath == "" );
     ASSERT_TRUE( info.arguments == "/c pause|echo this is a pause. please press a key" );
@@ -215,71 +213,96 @@ TEST_F(TestLNK, testCreateCustomLink)
   }
 }
 
-TEST_F(TestLNK, testLinkInfo)
+TEST_F(TestLNK, testWinXPSystemIni)
 {
-  {
-    lnk::LinkInfo info;
-    bool success = false;
-    success = lnk::getLinkInfo("./tests/Simple.txt.lnk", info);
-    ASSERT_TRUE( success == true );
-    LinkInfoDebug d = {
-      info.target.c_str(),
-      info.networkPath.c_str(),
-      info.arguments.c_str(),
-      info.description.c_str(),
-      info.workingDirectory.c_str(),
-      info.customIcon.filename.c_str(),
-    };
-    ASSERT_TRUE( info.target == "C:\\Program Files\\PDFCreator\\History.txt" );
-    ASSERT_TRUE( info.networkPath == "" );
-    ASSERT_TRUE( info.arguments == "\"this is the arguments\"" );
-    ASSERT_TRUE( info.description == "this is my comment" );
-    ASSERT_TRUE( info.workingDirectory == "C:\\Program Files\\PDFCreator" );
-    ASSERT_TRUE( info.customIcon.filename == "%SystemRoot%\\system32\\SHELL32.dll" );
-    ASSERT_TRUE( info.customIcon.index == 5 );
-  }
+  //Build test case link file
+  std::string lnkFilePath = getTestLink();
 
-  {
-    lnk::LinkInfo info;
-    bool success = false;
-    success = lnk::getLinkInfo("./tests/system.ini.lnk", info);
-    ASSERT_TRUE( success == true );
-    LinkInfoDebug d = {
-      info.target.c_str(),
-      info.networkPath.c_str(),
-      info.arguments.c_str(),
-      info.description.c_str(),
-      info.workingDirectory.c_str(),
-      info.customIcon.filename.c_str(),
-    };
-    ASSERT_TRUE( info.target == "C:\\WINDOWS\\system.ini" );
-    ASSERT_TRUE( info.networkPath == "" );
-    ASSERT_TRUE( info.arguments == "" );
-    ASSERT_TRUE( info.description == "" );
-    ASSERT_TRUE( info.workingDirectory == "C:\\WINDOWS" );
-    ASSERT_TRUE( info.customIcon.filename == "" );
-    ASSERT_TRUE( info.customIcon.index == 0 );
-  }
+  lnk::LinkInfo info;
+  bool success = false;
+  success = lnk::getLinkInfo(lnkFilePath.c_str(), info);
+  ASSERT_TRUE( success == true );
+  ASSERT_TRUE( info.target == "C:\\WINDOWS\\system.ini" );
+  ASSERT_TRUE( info.networkPath == "" );
+  ASSERT_TRUE( info.arguments == "" );
+  ASSERT_TRUE( info.description == "" );
+  ASSERT_TRUE( info.workingDirectory == "C:\\WINDOWS" );
+  ASSERT_TRUE( info.customIcon.filename == "" );
+  ASSERT_TRUE( info.customIcon.index == 0 );
+}
 
-  {
-    lnk::LinkInfo info;
-    bool success = false;
-    success = lnk::getLinkInfo("./tests/usbdrive.txt.lnk", info);
-    ASSERT_TRUE( success == true );
-    LinkInfoDebug d = {
-      info.target.c_str(),
-      info.networkPath.c_str(),
-      info.arguments.c_str(),
-      info.description.c_str(),
-      info.workingDirectory.c_str(),
-      info.customIcon.filename.c_str(),
-    };
-    ASSERT_TRUE( info.target == "G:\\usbdrive.txt" );
-    ASSERT_TRUE( info.networkPath == "" );
-    ASSERT_TRUE( info.arguments == "" );
-    ASSERT_TRUE( info.description == "" );
-    ASSERT_TRUE( info.workingDirectory == "G:\\" );
-    ASSERT_TRUE( info.customIcon.filename == "" );
-    ASSERT_TRUE( info.customIcon.index == 0 );
-  }
+TEST_F(TestLNK, testWinXpLongFilename)
+{
+  //Build test case link file
+  std::string lnkFilePath = getTestLink();
+
+  lnk::LinkInfo info;
+  bool success = false;
+  success = lnk::getLinkInfo(lnkFilePath.c_str(), info);
+  ASSERT_TRUE( success == true );
+  ASSERT_TRUE( info.target == "M:\\thisisanextralongfoldername\\thisisasuperhugelongfilename\\thisisasuperlongfilename.txt" );
+  ASSERT_TRUE( info.networkPath == "\\\\d49ads02\\users$\\BEAUCHAMP.A3\\thisisanextralongfoldername\\thisisasuperhugelongfilename\\thisisasuperlongfilename.txt" );
+  ASSERT_TRUE( info.arguments == "" );
+  ASSERT_TRUE( info.description == "" );
+  ASSERT_TRUE( info.workingDirectory == "M:\\thisisanextralongfoldername\\thisisasuperhugelongfilename" );
+  ASSERT_TRUE( info.customIcon.filename == "" );
+  ASSERT_TRUE( info.customIcon.index == 0 );
+}
+
+TEST_F(TestLNK, testWinXpUsbDrive)
+{
+  //Build test case link file
+  std::string lnkFilePath = getTestLink();
+
+  lnk::LinkInfo info;
+  bool success = false;
+  success = lnk::getLinkInfo(lnkFilePath.c_str(), info);
+  ASSERT_TRUE( success == true );
+  ASSERT_TRUE( info.target == "G:\\usbdrive.txt" );
+  ASSERT_TRUE( info.networkPath == "" );
+  ASSERT_TRUE( info.arguments == "" );
+  ASSERT_TRUE( info.description == "" );
+  ASSERT_TRUE( info.workingDirectory == "G:\\" );
+  ASSERT_TRUE( info.customIcon.filename == "" );
+  ASSERT_TRUE( info.customIcon.index == 0 );
+}
+
+TEST_F(TestLNK, testWinXpComment)
+{
+  //Build test case link file
+  std::string lnkFilePath = getTestLink();
+
+  lnk::LinkInfo info;
+  bool success = false;
+  success = lnk::getLinkInfo(lnkFilePath.c_str(), info);
+  ASSERT_TRUE( success == true );
+  ASSERT_TRUE( info.target != "" );
+  ASSERT_TRUE( info.description == "this is my comment" );
+}
+
+TEST_F(TestLNK, testWinXpArguments)
+{
+  //Build test case link file
+  std::string lnkFilePath = getTestLink();
+
+  lnk::LinkInfo info;
+  bool success = false;
+  success = lnk::getLinkInfo(lnkFilePath.c_str(), info);
+  ASSERT_TRUE( success == true );
+  ASSERT_TRUE( info.target != "" );
+  ASSERT_TRUE( info.arguments == "\"this is the arguments\"" );
+}
+
+TEST_F(TestLNK, testWinXpIcon)
+{
+  //Build test case link file
+  std::string lnkFilePath = getTestLink();
+
+  lnk::LinkInfo info;
+  bool success = false;
+  success = lnk::getLinkInfo(lnkFilePath.c_str(), info);
+  ASSERT_TRUE( success == true );
+  ASSERT_TRUE( info.target != "" );
+  ASSERT_TRUE( info.customIcon.filename == "%SystemRoot%\\system32\\SHELL32.dll" );
+  ASSERT_TRUE( info.customIcon.index == 219 );
 }
